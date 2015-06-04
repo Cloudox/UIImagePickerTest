b381a51eff3cceb976c751693700768ab669f308
Cloudox <cloudox@foxmail.com>
2015-06-04 17:45:47 +0800

    Initial Commit

---
+++ ImagePickerTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImagePickerTest
+//
+//  Created by mac on 15/6/4.
+//  Copyright (c) 2015年 Cloudox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImagePickerTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImagePickerTest
+//
+//  Created by mac on 15/6/4.
+//  Copyright (c) 2015年 Cloudox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
