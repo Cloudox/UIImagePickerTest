500c12e217c170a33e7d28bc6d17c16255c7d48f
Cloudox <cloudox@foxmail.com>
2015-06-08 17:57:29 +0800

    添加imageView，添加按钮响应actionSheet

--- ImagePickerTest/ViewController.h
+++ ImagePickerTest/ViewController.h
@@ -8,8 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UIActionSheetDelegate>
 
+- (IBAction)changeAvata:(id)sender;
 
 @end
 
--- ImagePickerTest/ViewController.m
+++ ImagePickerTest/ViewController.m
@@ -24,4 +24,14 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)changeAvata:(id)sender {
+    UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:@"更换头像" delegate:self cancelButtonTitle:@"取消" destructiveButtonTitle:nil otherButtonTitles:@"拍照", @"从相册选择", nil];
+    //actionSheet风格，感觉也没什么差别- -
+    actionSheet.actionSheetStyle = UIActionSheetStyleDefault;//默认风格，灰色背景，白色文字
+//    actionSheet.actionSheetStyle = UIActionSheetStyleAutomatic;
+//    actionSheet.actionSheetStyle = UIActionSheetStyleBlackTranslucent;
+//    actionSheet.actionSheetStyle = UIActionSheetStyleBlackOpaque;//纯黑背景，白色文字
+    
+    [actionSheet showInView:self.view];
+}
 @end
